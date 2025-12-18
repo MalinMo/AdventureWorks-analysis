c17215054ecd09dee09e3995f98423139f1f0cf5
Malin Moisander <malin.moisander@gmail.com>
2025-12-18 10:05:42 +0100

    Visulaisering VG1 tillagd

---
+++ data/visVG1.sql
@@ -0,0 +1,24 @@
+USE AdventureWorks2025;
+
+SELECT TOP 10 * FROM Sales.SalesTerritory;
+SELECT TOP 10 * FROM Sales.SalesOrderHeader;
+
+SELECT
+    CASE
+        WHEN st.CountryRegionCode = 'US'
+            THEN CONCAT('US ',st.Name)
+        ELSE st.Name
+    END AS Region,
+    SUM(soh.TotalDue) AS TotalSales,
+    COUNT(DISTINCT soh.SalesOrderID) AS NumberOfOrders,
+    SUM(soh.TotalDue) / NULLIF(COUNT(DISTINCT soh.SalesOrderID), 0) AS AvgTotalOrderValue
+FROM Sales.SalesTerritory AS st
+LEFT JOIN Sales.SalesOrderHeader AS soh
+    ON st.TerritoryID = soh.TerritoryID
+GROUP BY
+    CASE
+        WHEN st.CountryRegionCode = 'US'
+            THEN CONCAT('US ',st.Name)
+        ELSE st.Name
+    END
+ORDER BY AvgTotalOrderValue DESC;
